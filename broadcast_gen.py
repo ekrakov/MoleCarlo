0214c04de0703169cc83de1bbd5a3b9f76f9f474
ek2992 <36238899+ek2992@users.noreply.github.com>
2024-11-29 13:00:28 -0800

    seperate functions for speed

--- broadcast_gen.py
+++ broadcast_gen.py
@@ -1,8 +1,11 @@
-from dis import dis
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 from mpmath import mp
+from utils.get_compcell import setup_cell
+import time
+
+
 plt.style.use("~/.matplotlib/styles/style.mplstyle")
 
 # np.set_printoptions(precision=17)
@@ -31,12 +34,13 @@ def generate_periodic(atoms,rcut,latvec,lmax=None, mmax=None, nmax=None):
 
     l_disp, m_disp, n_disp = np.meshgrid(l_disp_range, m_disp_range, n_disp_range, indexing='ij')
     all_displacements = np.stack([l_disp.ravel(), m_disp.ravel(), n_disp.ravel()], axis=-1)  # Shape: (num_displacements, 3)
+    displacement_extended = all_displacements[np.newaxis, :, :]@latvec
 
-    return all_displacements
+    return displacement_extended
 
 
 
-def periodic(atoms,rcut,latvec,vacancy=False):
+def periodic(atoms,rcut,displacement_extended,vacancy=False):
     """
     Periodic boundary conditions when many coppies are required (rcut)
     Parameters:
@@ -50,19 +54,7 @@ def periodic(atoms,rcut,latvec,vacancy=False):
 
     if vacancy:
         atoms = np.delete(atoms, 2, axis=0)
-    
-  
 
-            
-    # lmax = int(np.ceil(rcut / latvec[0, 0])) 
-    # mmax = int(np.ceil(rcut / latvec[1, 1])) 
-    # nmax = int(np.ceil(rcut / latvec[2, 2])) 
-
-    print(lmax,'here')
-    # Generate periodic image displacement vectors for each axis
-    l_disp_range = np.arange(-lmax, lmax + 1)
-    m_disp_range = np.arange(-mmax, mmax + 1)
-    n_disp_range = np.arange(-nmax, nmax + 1)
 
     rcut_sqr=rcut*rcut
     rcut_6=rcut_sqr*rcut_sqr*rcut_sqr
@@ -70,17 +62,12 @@ def periodic(atoms,rcut,latvec,vacancy=False):
     ecut=(-1/rcut_6+1/rcut_12)
 
     # Create a 3D grid of displacements
-    l_disp, m_disp, n_disp = np.meshgrid(l_disp_range, m_disp_range, n_disp_range, indexing='ij')
-    all_displacements = np.stack([l_disp.ravel(), m_disp.ravel(), n_disp.ravel()], axis=-1)  # Shape: (num_displacements, 3)
-
-
+  
 
     atoms_extended = atoms[:, np.newaxis, :]  # Shape (N, 1, 3) (Expand atoms in computational cell)
-    displacement_extended = all_displacements[np.newaxis, :, :]@latvec  # Shape (1, n_disp, 3) Expand displacements to compute all periodic displacements with atoms expanded
    
     displacement_vec =-atoms_extended + displacement_extended #shape (n_atom,disp,3) (all displacements for every atom)
     flattened_displacements = displacement_vec.reshape(-1, 3) # I do not care what atom the displacement came from. 
-    all_displacements = np.stack([l_disp.ravel(), m_disp.ravel(), n_disp.ravel()], axis=-1)  # Shape: (num_displacements, 3)
 
 
     ###########################START PAIRWISE###########################################
@@ -92,7 +79,7 @@ def periodic(atoms,rcut,latvec,vacancy=False):
     disp_diff=atoms_extended+disp_expanded_pairwise
   
     #### get pairwise distance between all atoms 
-    pairwise_distances = np.linalg.norm(disp_diff, axis=-1).astype(np.longdouble)
+    pairwise_distances = np.linalg.norm(disp_diff, axis=-1)
     #janky filtering methods 
     pairwise_distances[pairwise_distances <= 0] = np.inf
     pairwise_distances[pairwise_distances >rcut] = np.inf
@@ -130,9 +117,6 @@ def periodic(atoms,rcut,latvec,vacancy=False):
 
 
 
-
-
-
 #############DEFINE VARIABLES ##################################
 
 # L=1
@@ -147,39 +131,58 @@ def periodic(atoms,rcut,latvec,vacancy=False):
 #     [0, 0, N * lattice]
 # ])
 
-atoms,lv=setup_cell(2,2,2)
+start_time = time.time()
+atoms, lv = setup_cell(2, 2, 2)
+setup_cell_time = time.time() - start_time
+
+# Measure time for generate_periodic
+start_time = time.time()
+disp_extended = generate_periodic(atoms, 1.3, lv, lmax=None, mmax=None, nmax=None)
+generate_periodic_time = time.time() - start_time
+
+# Measure time for periodic
+start_time = time.time()
+pot, force = periodic(atoms, 1.3, disp_extended)
+periodic_time = time.time() - start_time
+
+# Output results
+print(f"setup_cell time: {setup_cell_time:.6f} seconds")
+print(f"generate_periodic time: {generate_periodic_time:.6f} seconds")
+print(f"periodic time: {periodic_time:.6f} seconds")
+print(f"Potential Energy: {pot}, here")
+
 # # rcut=2.2
 
 
 # # print(vector_indices)
 
 
-num_atoms = atoms.shape[0]
+# num_atoms = atoms.shape[0]
 
-# start_time = time.time()
-pot,force=periodic(atoms,1.3,lv)
-print(pot/(num_atoms-1))
-print(pot)
+# # start_time = time.time()
+# pot,force=periodic(atoms,1.3,lv)
+# print(pot/(num_atoms-1))
+# print(pot)
 
-e_arr = []
-L_values = []
+# e_arr = []
+# L_values = []
 
-# Loop for m = 1 to 5
-for i in range(8):
-    m = i + 1
-    atoms, lv = setup_cell(m, m, m)
-    num_atoms = atoms.shape[0] - 1
-    pot, force = periodic(atoms, 1.3, lv)
-    L_values.append(m)
-    print(pot/num_atoms)
-    print(num_atoms,'here shape')
-    e_arr.append(pot / num_atoms)
+# # Loop for m = 1 to 5
+# for i in range(8):
+#     m = i + 1
+#     atoms, lv = setup_cell(m, m, m)
+#     num_atoms = atoms.shape[0] - 1
+#     pot, force = periodic(atoms, 1.3, lv)
+#     L_values.append(m)
+#     print(pot/num_atoms)
+#     print(num_atoms,'here shape')
+#     e_arr.append(pot / num_atoms)
 
-# Plot L (L=M=N) vs Energy/Atom
-plt.figure(figsize=(8, 6))
-plt.plot(L_values, e_arr, marker='o', linestyle='-')
-plt.axhline(y=-6, color='black', linestyle='--', label="y = -6")  
-plt.xlabel("$(L=M=N)$")
-plt.ylabel("Energy/Atom")
-plt.savefig("energy_vs_lattice.pdf", format="pdf", bbox_inches="tight")
-plt.show()
+# # Plot L (L=M=N) vs Energy/Atom
+# plt.figure(figsize=(8, 6))
+# plt.plot(L_values, e_arr, marker='o', linestyle='-')
+# plt.axhline(y=-6, color='black', linestyle='--', label="y = -6")  
+# plt.xlabel("$(L=M=N)$")
+# plt.ylabel("Energy/Atom")
+# plt.savefig("energy_vs_lattice.pdf", format="pdf", bbox_inches="tight")
+# plt.show()
